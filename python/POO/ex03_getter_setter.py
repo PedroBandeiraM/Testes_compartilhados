d288ab36d5eadf26774fe2e74eea4823d0094030
Pedro Mari <pedrobmari@gmail.com>
2026-01-16 12:09:50 -0300

    bla6

--- python/POO/ex03_getter_setter.py
+++ python/POO/ex03_getter_setter.py
@@ -17,3 +17,28 @@ class Aluno:
 aluno_1 = Aluno("Paulo", "7,5")
 
 print(aluno_1.media)
+
+# -----------------------------------------------------------------------------------------------
+
+class Carta:
+    def __init__(self, paises):
+        self.selos = paises
+
+    @property
+    def selos(self): #Retorna uma lista com as siglas dos países
+        siglas = []
+        for i in range(len(self._selos)):
+            siglas.append(self._selos[i][:2])
+        return siglas
+    
+    @selos.setter
+    def selos(self, paises): # Valida os dados: Capitaliza as strings
+        for i in range(len(paises)):
+            paises[i] = paises[i].capitalize()
+        self._selos = paises
+
+pacote_1 = Carta(["brasil", "rÚssIa", "índia", "MaLawi"])
+
+print(f"\nSelos de países (com getter): {pacote_1.selos}") 
+print(f"Selos de países (sem getter): {pacote_1._selos} \n")
+
