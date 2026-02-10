dbb862cac51aaeb2b8b45ab1e39f87177c594910
Pedro Mari <pedrobmari@gmail.com>
2026-02-10 15:00:24 -0300

    criacao de arquivo java e testes iniciais com a linguagem

---
+++ Java/Conceitos básicos/EntradaSaida.java
@@ -0,0 +1,10 @@
+public class EntradaSaida {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }    
+
+    // Forma aceitável, mas não recomendável
+    void main() {
+        IO.println("Hello, world!");
+    }    
+}
