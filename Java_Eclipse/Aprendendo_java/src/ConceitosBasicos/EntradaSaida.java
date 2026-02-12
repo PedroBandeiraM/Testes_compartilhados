158fa85eaeabe600c19a1cbdf99f95b8459ece2e
Pedro Mari <pedrobmari@gmail.com>
2026-02-12 12:37:33 -0300

    Adiciona novos arquivos Java e Eclipse

---
+++ Java_Eclipse/Aprendendo_java/src/ConceitosBasicos/EntradaSaida.java
@@ -0,0 +1,11 @@
+package ConceitosBasicos;
+
+public class EntradaSaida {
+
+	public static void main(String[] args) {
+		System.out.println("Java é uma merda");
+		System.out.println("Eclipse consegue ser pior");
+		
+	}
+
+}
