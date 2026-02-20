b8c4a033fcd8066067b0f443f31d084dd640c1f3
Pedro Mari <pedrobmari@gmail.com>
2026-02-20 14:43:02 -0300

    Exercício: 1 POO

---
+++ Java/Estudo_Java_Eclipse_POO/src/entities/B_class_Retangulo.java
@@ -0,0 +1,24 @@
+package entities;
+
+public class B_class_Retangulo {
+	double altura;
+	double largura;
+	
+	public B_class_Retangulo(double altura, double largura) {
+		this.altura = altura;
+		this.largura = largura;
+	}
+	
+	public double perimetro() {
+		return (2 * altura) + (2 * largura);
+	}
+	
+	public double area() {
+		return (altura * largura);
+	}
+	
+	public double diagonal() {
+		return Math.sqrt(Math.pow(altura, 2) + Math.pow(largura, 2));
+	}
+	
+}
---
+++ Java/Estudo_Java_Eclipse_POO/src/programs/B_Retangulo.java
@@ -0,0 +1,33 @@
+package programs;
+
+import java.util.Scanner;
+import entities.B_class_Retangulo;
+
+public class B_Retangulo {
+
+	public static void main(String[] args) {
+		Scanner leitor = new Scanner(System.in);
+		
+		System.out.print(" - Digite a altura do retângulo: ");
+		double altura = leitor.nextDouble();
+		
+		System.out.print(" - Digite a largura do retângulo: ");
+		double largura = leitor.nextDouble();
+		
+		B_class_Retangulo retangulo = new B_class_Retangulo(altura, largura);
+		
+		double perimetro = retangulo.perimetro();
+		double area = retangulo.area();
+		double diagonal = retangulo.diagonal();
+		
+		System.out.printf("""
+				%n -> Dados do retângulo:
+				    - Perímetro: %.2f
+				    - Área: %.2f
+				    - Diagonal %.2f
+				""", perimetro, area, diagonal);
+		
+		leitor.close();
+	}
+
+}
